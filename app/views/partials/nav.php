9e7c8141ec3ffb253a8e5fd0779ba138b2ecd7a2
Ken Studdy <kenstuddy@users.noreply.github.com>
2017-10-11 18:50:40 -0400

    Removed whitespace

--- app/views/partials/nav.php
+++ app/views/partials/nav.php
@@ -1,6 +1,6 @@
-    <nav>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/users">Users</a></li>
-        <li><a href="/contact">Contact</a></li>
-    </nav>
+<nav>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">About</a></li>
+    <li><a href="/users">Users</a></li>
+    <li><a href="/contact">Contact</a></li>
+</nav>
