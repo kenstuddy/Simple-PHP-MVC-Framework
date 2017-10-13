9a110bf9bc488cf0df3d16747414626f6bb2c3b2
Ken Studdy <kenstuddy@users.noreply.github.com>
2017-10-13 14:38:34 -0400

    Added commenting

--- Config.php
+++ Config.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * This is where configuration information is stored about the framework. We can add extra options such as the PDO error mode,
+ * PDO timeout, or any other attributes that may be useful.
+ */
 return [
     'database' => [
         'name' => 'name',
