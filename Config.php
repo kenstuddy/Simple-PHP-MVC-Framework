e72098194c430a1b5e2ee308f44ac3ac6c5a0adc
Ken Studdy <kenstuddy@users.noreply.github.com>
2017-11-12 06:51:50 -0500

    Added PDO natural case
    I have added PDO natural case so that the case returned for column names is in the case the database driver returned them, and not the default (all uppercase on some PDO extensions).

--- Config.php
+++ Config.php
@@ -10,7 +10,8 @@ return [
         'password' => 'password',
         'connection' => 'mysql:host=127.0.0.1',
         'options' => [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_CASE => PDO::CASE_NATURAL
         ]
     ]
 ]
