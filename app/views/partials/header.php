64745c321242917fb35369b43a0589371fa4f331
Ken Studdy <kenstuddy@users.noreply.github.com>
2020-08-03 18:15:27 -0400

    Added title to header

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -2,9 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <title>Document</title>
-   	<link rel="stylesheet" href="css/bootstrap.min.css">
-  	<script src="js/bootstrap.min.js"></script>
+    <title>Simple PHP MVC Framework</title>
+   	<link rel="stylesheet" href="/css/bootstrap.min.css">
+  	<script src="/js/jquery.min.js"></script>
+  	<script src="/js/popper.min.js"></script>
+  	<script src="/js/bootstrap.min.js"></script>
 </head>
 <body>
 <div class="container">
