326b6179b467900a5566a942eb74e96e7c55e8b9
Ken Studdy <kenstuddy@gmail.com>
2021-04-10 20:40:36 -0400

    Moved scripts to footer

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -1,3 +1,7 @@
 </div>
 </body>
+<script src="/js/main.js"></script>
+<script src="/js/jquery.min.js"></script>
+<script src="/js/popper.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
 </html>
--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -6,10 +6,6 @@
    	<link rel="stylesheet" href="/css/bootstrap.min.css">
    	<link rel="stylesheet" href="/css/main.css">
     <script src="/js/dark-toggle.js"></script>
-    <script src="/js/main.js"></script>
-  	<script src="/js/jquery.min.js"></script>
-  	<script src="/js/popper.min.js"></script>
-  	<script src="/js/bootstrap.min.js"></script>
 </head>
 <body class="bg-white" onload="loadDarkMode()">
 <div id="darkmode" style="display:none"></div>
