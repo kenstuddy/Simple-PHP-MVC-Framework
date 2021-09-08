4e554af8d5060a556358fe02742bfb81884e53e8
Ken Studdy <kenstuddy@users.noreply.github.com>
2021-09-07 20:58:53 -0400

    Fixed error page dark mode

--- app/views/error.view.php
+++ app/views/error.view.php
@@ -1,6 +1,6 @@
 <?php require ('partials/header.php'); ?>
-<h1 class="text-dark">An Error Has Occurred</h1>
-<p class="text-dark">You have encountered an error. Please click <a href="/">here</a> to go to the home page.</p>
+<h1 class="<?= theme('text-white-75','text-dark') ?>">An Error Has Occurred</h1>
+<p class="<?= theme('text-white-75','text-dark') ?>">You have encountered an error. Please click <a href="/">here</a> to go to the home page.</p>
 <?php if (isset($error)): ?>
     <?php echo $error; ?>
 <?php endif; ?>
