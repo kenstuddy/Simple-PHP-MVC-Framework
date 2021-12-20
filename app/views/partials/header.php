c4001ccb0c753ffec518e49ff80a424c880666dc
Ken Studdy <kenstuddy@users.noreply.github.com>
2021-12-19 21:55:50 -0500

    Added meta viewport

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -1,7 +1,8 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simple PHP MVC Framework</title>
    	<link rel="stylesheet" href="/css/bootstrap.min.css">
    	<link rel="stylesheet" href="/css/main.css">
