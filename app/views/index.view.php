c9f5fd125473dcd8f6079233dcc4801442b50336
Ken Studdy <kenstuddy@users.noreply.github.com>
2022-03-23 22:41:15 -0400

    Added meta description

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -1,5 +1,5 @@
 <?php require ('partials/header.php'); ?>
 <h1 class="<?= theme('text-white-75', 'text-dark') ?>">Home Page</h1>
 <p class="<?= theme('text-white-75', 'text-dark') ?> home-paragraph">Welcome to the Simple PHP MVC Framework! Simple PHP MVC Framework is a fast PHP MVC framework designed to allow for easy creation of powerful websites and web applications. It is inspired by <a class="<?= theme('text-light', 'text-primary') ?>" href="https://laravel.com/">Laravel</a>, and is designed to be easy to use.</p>
-<img src="/img/logo.png" class="home-logo" />
+<img src="/img/logo.png" alt="Simple PHP MVC Framework Logo" class="home-logo" />
 <?php require ('partials/footer.php'); ?>
--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Lightweight fast PHP framework Simple PHP MVC Framework">
     <title>Simple PHP MVC Framework</title>
    	<link rel="stylesheet" href="/css/bootstrap.min.css">
    	<link rel="stylesheet" href="/css/main.css">
