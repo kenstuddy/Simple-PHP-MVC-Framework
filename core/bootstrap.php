39386d0b0a4e45b42c9bc35473992c8fac4ac126
Ken Studdy <kenstuddy@users.noreply.github.com>
2023-11-28 21:27:02 -0500

    Switched to config.php.example

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -3,10 +3,16 @@
 use App\Core\App;
 use App\Core\Database\{QueryBuilder, Connection};
 
+$configFilePath = __DIR__ . '/../config.php';
+$configExampleFilePath = __DIR__ . '/../config.php.example';
+
+if (!file_exists($configFilePath) && file_exists($configExampleFilePath)) {
+    copy($configExampleFilePath, $configFilePath);
+}
 
 require 'helpers.php';
 
-App::bind('config', require '../config.php');
+App::bind('config', require $configFilePath);
 
 App::bind('database', new QueryBuilder(
     Connection::make(App::get('config')['database'])
